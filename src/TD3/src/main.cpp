935af6852397a2f4f1d6afc9f9b657e0cf3244be
LouisePrd <louise.peridy22@gmail.com>
2024-03-31 23:53:42 +0200

    debut ex3 optionnel

--- src/TD3/src/main.cpp
+++ src/TD3/src/main.cpp
@@ -1,12 +1,10 @@
 #include "token.hpp"
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <sstream>
 #include <string>
 #include <iterator>
 #include <cctype>
-
 
 std::vector<std::string> split_string(std::string const &s)
 {
@@ -19,7 +17,7 @@ float npi_evaluate(std::vector<std::string> const &tokens)
     std::vector<float> stack;
     for (std::string const &token : tokens)
     {
-        if (is_floating(token)) // on regarde si nombre ou opérande
+        if (is_floating(token))
         {
             stack.push_back(std::stof(token));
         }
@@ -51,22 +49,51 @@ float npi_evaluate(std::vector<std::string> const &tokens)
     return stack.back();
 }
 
+size_t operator_precedence(Operator const op) // on donne la priorité des opérateurs
+{
+    switch (op)
+    {
+    case Operator::ADD:
+        return 1;
+    case Operator::SUB:
+        return 1;
+    case Operator::MUL:
+        return 2;
+    case Operator::DIV:
+        return 2;
+    default:
+        return 0;
+    }
+}
+
+std::vector<Token> infix_to_npi_tokens(std::string const &expression)
+{
+    std::vector<Token> output;
+    output = tokenize(split_string(expression));
+    return output;
+}
+
 int main()
 {
     // Exercice 1
     /*std::string operation;
-    std::cout << "Entrez l'operation a evaluer en NPI V1 :";
+    std::cout << "Entrez l'operation a evaluer en NPI V1 : ";
     getline(std::cin, operation);
     std::vector<std::string> elements = split_string(operation);
     std::cout << "Resultat : " << npi_evaluate(elements) << std::endl;*/
 
     // Exercice 2
-    std::string operation2;
-    std::cout << "Entrez l'operation a evaluer en NPI V2 :";
+    /*std::string operation2;
+    std::cout << "Entrez l'operation a evaluer en NPI V2 : ";
     getline(std::cin, operation2);
     std::vector<std::string> elements2 = split_string(operation2);
-    std::cout << "Resultat : " << npi_evaluate(tokenize(elements2)) << std::endl;
+    std::cout << "Resultat : " << npi_evaluate(tokenize(elements2)) << std::endl;*/
 
-
+    //  Exercice 3
+    std::string operation3;
+    std::cout << "Conversion en NPI : ";
+    getline(std::cin, operation3);
+    std::vector<std::string> elements3 = split_string(operation3);
+    std::vector<Token> output = infix_to_npi_tokens(operation3);
     return 0;
 }
