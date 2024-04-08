c89b3a2785bb1c257db4273a3fa5cbe6a1ff689b
LouisePrd <louise.peridy22@gmail.com>
2024-04-08 22:24:24 +0200

    TD5 ex2 debut

--- src/TD5/src/main.cpp
+++ src/TD5/src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <numeric>
 #include <unordered_map>
+#include <cstdlib>
 
 size_t folding_string_hash(std::string const &s, size_t max)
 {
@@ -53,6 +53,40 @@ size_t polynomial_rolling_hash(const std::string& s, size_t p, size_t m)
     return hash;
 }
 
+
+// ROBOT
+
+std::string random_name(size_t size) {
+    std::string name {""};
+    // Optimisation pour que la chaîne de caractère ne réalloue pas de la mémoire à chaque caractère ajouté
+    // https://cplusplus.com/reference/string/string/reserve/
+    name.reserve(size);
+    for(size_t i {0}; i < size; ++i) {
+        name.push_back('A' + std::rand() % 26);
+    }
+    return name;
+}
+
+std::vector<std::pair<std::string, float>> get_robots_fix(size_t size) {
+    std::vector<std::pair<std::string, float>> robots_fix {};
+    // Meme optimisation que dans random_name()
+    // https://cplusplus.com/reference/vector/vector/reserve/
+    robots_fix.reserve(size);
+    for (size_t i {0}; i < size; ++i) {
+        // random name 
+        std::string robotName { random_name(2) };
+        // random cost
+        float cost {static_cast<float>(std::rand()) / RAND_MAX * 1000.0f};
+        robots_fix.push_back(std::make_pair(robotName, cost));
+    }
+    return robots_fix;
+}
+
+std::unordered_map<std::string, std::vector<float>> robots_fixes_map(std::vector<std::pair<std::string, float>> const& robots_fixes) {
+    std::unordered_map<std::string, std::vector<float>> robots_fixes_map {};
+    return robots_fixes_map;
+}
+
 int main()
 {
     // Exercice 1 (fonction de hachage)
@@ -65,6 +99,11 @@ int main()
     // Question 3
     polynomial_rolling_hash("abc", 31, 1024);
     polynomial_rolling_hash("cab", 31, 1024);
+
+    // Exercice 2 (Réparation de Robots)
+    std::cout << "Test nom robot : " << random_name(2) << std::endl;
+    std::vector<std::pair<std::string, float>> robots_fix = get_robots_fix(10);
+    std::unordered_map<std::string, std::vector<float>> robots_fixes = robots_fixes_map(robots_fix);
 
     return 0;
 }
