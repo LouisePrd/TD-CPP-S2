4a2707923eebcc645825a09323ae57228d0d81b3
LouisePrd <louise.peridy22@gmail.com>
2024-04-16 14:09:23 +0200

    quest 1 2 et 3 ex1

--- src/TD6/src/node.cpp
+++ src/TD6/src/node.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
 #include "node.hpp"
+
+void pretty_print_left_right(Node const &node, std::string const &prefix, bool is_left)
+{
+    if (node.right)
+    {
+        pretty_print_left_right(*node.right, prefix + (is_left ? "|   " : "    "), false);
+    }
+    std::cout << prefix << (is_left ? "+-- " : "+-- ") << node.value << std::endl;
+    if (node.left)
+    {
+        pretty_print_left_right(*node.left, prefix + (is_left ? "    " : "|   "), true);
+    }
+}
+
+void pretty_print_left_right(Node const &node)
+{
+    pretty_print_left_right(node, "", true);
+}
+
+Node *create_node(int value)
+{
+    Node *newNode{new Node{value, nullptr, nullptr}};
+    return newNode;
+}
+
+bool Node::is_leaf() const
+{
+    return (left == nullptr && right == nullptr);
+}
+
+void Node::insert(int value)
+{
+    if (value < this->value)
+        left = create_node(value);
+    else
+        right = create_node(this->value);
+}
+
+int Node::height() const
+{
+    int heigh;
+    while (left != nullptr)
+    {
+        heigh++;
+    }
+    return heigh;
+}
---
+++ src/TD6/src/node.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+struct Node {
+    int value;
+    Node* left { nullptr };
+    Node* right { nullptr };
+
+    bool is_leaf() const;
+    void insert(int value);
+    int height() const;
+};
+
+void pretty_print_left_right(Node const& node, std::string const& prefix, bool is_left);
+void pretty_print_left_right(Node const& node);
+Node* create_node(int value);
