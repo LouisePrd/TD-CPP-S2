96e9858a50565fc4dc63cbe69ed30e69682b2575
LouisePrd <louise.peridy22@gmail.com>
2024-06-07 20:56:11 +0200

    add quicksort TD2

--- src/TD2/src/main.cpp
+++ src/TD2/src/main.cpp
@@ -23,11 +23,38 @@ void bubble_sort(std::vector<int> &vec)
 }
 
 // Exercice 2 (tri récursif)
-void merge_sort_merge(std::vector<float> & vec, size_t const left, size_t const middle, size_t const right);
-void merge_sort(std::vector<float> & vec, size_t const left, size_t const right);
-void merge_sort(std::vector<float> & vec) {
-    merge_sort(vec, 0, vec.size() - 1);
+size_t quick_sort_partition(std::vector<float> & vec, size_t left, size_t right, size_t const pivot)
+{
+    std::swap(vec[pivot], vec[right]);
+    int pivotValue = vec[right];
+    size_t index = left;
+
+    for (size_t i = left; i < right; i++) {
+        if (vec[i] < pivotValue) {
+            std::swap(vec[i], vec[index]);
+            index++;
+        }
+    }
+    std::swap(vec[index], vec[right]);
+    return index;
 }
+
+void quick_sort(std::vector<float> & vec, size_t const left, size_t const right) {
+    if (left < right) {
+        size_t pivot = (left + right) / 2;
+        pivot = quick_sort_partition(vec, left, right, pivot);
+        if (pivot > 0) {
+            quick_sort(vec, left, pivot - 1);
+        }
+        quick_sort(vec, pivot + 1, right);
+    }
+}
+
+
+void quick_sort(std::vector<float> & vec) {
+    quick_sort(vec, 0, vec.size() - 1);
+}
+
 
 int main()
 {
@@ -36,11 +63,17 @@ int main()
     bubble_sort(array);
     for (unsigned int i = 0; i < array.size(); i++)
     {
-        std::cout << array[i] << ", ";
+        //std::cout << array[i] << ", ";
     }
 
     // Tri fusion
-    std::vector<int> array2{1, 2, 4, 5, 6, 12, 8, 9, 3, 3, 0};
+    std::vector<float> array2{1, 9, 4, 5, 6, 12, 8, 9, 3, 3, 0};
+    int size = array2.size();
+    quick_sort(array2);
+    for (unsigned int i = 0; i < array2.size(); i++)
+    {
+        std::cout << array2[i] << ", ";
+    }
 
     return 0;
 }
